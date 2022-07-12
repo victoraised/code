8a97c3122b5b013f2acba7f9a2e84a08457a8a27
Aisedion <107373938+victoraised@users.noreply.github.com>
2022-07-12 02:01:19 +0100

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,37 @@
+
+function displayTime() {
+    var dateTime = new Date();
+   var hrs = dateTime.getHours();
+   var min = dateTime.getMinutes();
+   var sec = dateTime.getSeconds();
+    var session = document.getElementById('session');
+  
+  if(hrs >= 24){
+    session.innerHTML = 'PM';
+  }else{
+    session.innerHTML = 'AM';
+  }
+  if(hrs > 24){
+    hrs = hrs - 24;
+    }
+  document.getElementById('hours').innerHTML = hrs;
+  document.getElementById('minutes').innerHTML = min;
+  document.getElementById('seconds').innerHTML = sec;
+}
+  setInterval(displayTime, 10);
+  
+var menulist = document.getElementById("menulist");
+menulist.style.maxHeight = "0px";
+
+function togglemenu() {
+  if (menulist.style.maxHeight == "0px")
+  {
+    menulist.style.maxHeight = "130px"
+  }
+  else
+  {
+    menulist.style.maxHeight = "0px"
+
+  }
+}
+
